845aeac2d2f8e0e587a48e9212777b833f1843fb
richard slettevoll <richard_slettevoll@hotmail.com>
2022-02-23 00:23:58 +0100

    last minute fixes

--- main.ts
+++ main.ts
@@ -1,81 +1,172 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+// TODO: fix at entries ikke dokke opp på feil år
 
-interface MyPluginSettings {
+interface HeatmapCalendarSettings {
 	mySetting: string;
+	year: number;
+	intensity: number;
+	colors: {
+		default: Array<string>;
+	};
+	entries: Array<Entry>;
+}
+const DEFAULT_SETTINGS: HeatmapCalendarSettings = {
+	mySetting: 'default',
+	year: new Date().getFullYear(),
+	intensity: 4,
+	colors: {
+		default: ["#c6e48b","#7bc96f","#49af5d","#2e8840","#196127"]
+	},
+	entries: [{date: "1900-01-01"}]
+}
+/*
+colors: {
+        default: ["#c6e48b","#7bc96f","#49af5d","#2e8840","#196127"],//green
+        blue: ["#8cb9ff","#69a3ff","#428bff","#1872ff","#0058e2"],
+        red: ["#ff9e82","#ff7b55","#ff4d1a","#e73400","#bd2a00"],
+        orange: ["#ffa244","#fd7f00","#dd6f00","#bf6000","#9b4e00"],
+        pink: ["#ff96cb","#ff70b8","#ff3a9d","#ee0077","#c30062"],
+        orangeToRed: ["#ffdf04","#ffbe04","#ff9a03","#ff6d02","#ff2c01"]
+
+    },
+	*/
+
+interface Entry {
+	date: string;
+	intensity?: number;
+	color?: string | number;
+	content?: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface CalendarData {
+	year?: number;
+	colors?: {
+		[index: string | number]: {
+			[index: number]: string;
+		};
+	};
+	entries?: Array<Entry>;
+
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const calendarData: CalendarData = {
+	year: 2022,
+	colors: {
+		default: ["#c6e48b", "#7bc96f", "#49af5d", "#2e8840", "#196127"],
+		blue: ["#8cb9ff", "#69a3ff", "#428bff", "#1872ff", "#0058e2"],
+	},
+	entries: [
+		{
+			date: "2022-12-31",
+			intensity: 5,
+			color: "blue",
+			content: "blue",
+		},
+		{
+			date: "2022-12-31",
+			intensity: 5,
+			color: "blue",
+			content: "blue",
+		},
+	]
+}
+
+export default class HeatmapCalendar extends Plugin {
+
+	settings: HeatmapCalendarSettings;
+
+	daysIntoYear(date: Date): number {
+		return (
+			(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) -
+				Date.UTC(date.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000
+		)
+	}
 
 	async onload() {
+
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		//@ts-ignore
+		window.renderHeatmapCalendar = (el: HTMLElement, calendarData: CalendarData): void => {
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+			const year = calendarData.year || this.settings.year
+			const colors = calendarData.colors || this.settings.colors
+			const calEntries = calendarData.entries || this.settings.entries
+			!calEntries[0].intensity && this.settings.intensity
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+			//map() ?
+			const entries: Array<Entry> = []
+			calEntries.forEach(e => {
+				entries[this.daysIntoYear(new Date(e.date))] = e
+			})
+
+			const firstDayOfYear = new Date(Date.UTC(year, 0, 1))
+			let numberOfEmptyDaysBeforeYearBegins = (firstDayOfYear.getDay() + 5) % 6
+			let boxes = ""
+			while (numberOfEmptyDaysBeforeYearBegins) {
+				boxes += `<li style="background-color: transparent"></li>`
+				numberOfEmptyDaysBeforeYearBegins--
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+
+			const lastDayOfYear = new Date(Date.UTC(year, 11, 31))
+			const numberOfDays = this.daysIntoYear(lastDayOfYear) //eg 365 or 366
+			for (let day = 1; day <= numberOfDays; day++) {
+
+				let background_color, content = ""
+
+				if (entries[day]) {
+
+					if (entries[day].color) {
+						background_color = colors[entries[day].color][entries[day].intensity - 1]
+					} else { // default color: first color in colors array
+						background_color = colors.default[entries[day].intensity - 1]
+					}
+					if (entries[day].content) {
+						content = entries[day].content
 					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+					boxes += `<li style="background-color:${background_color};">${content}</li>`
+
+				} else {
+					boxes += `<li></li>`
 				}
 			}
-		});
+			const html = `
+				<div class="heatmap-calendar-graph">
+				<div class="heatmap-calendar-year">${String(year).slice(2)}</div>
+				<ul class="heatmap-calendar-months">
+					<li>Jan</li>
+					<li>Feb</li>
+					<li>Mar</li>
+					<li>Apr</li>
+					<li>May</li>
+					<li>Jun</li>
+					<li>Jul</li>
+					<li>Aug</li>
+					<li>Sep</li>
+					<li>Oct</li>
+					<li>Nov</li>
+					<li>Dec</li>
+				</ul>
+				<ul class="heatmap-calendar-days">
+					<li>Mon</li>
+					<li>Tue</li>
+					<li>Wed</li>
+					<li>Thu</li>
+					<li>Fri</li>
+					<li>Sat</li>
+					<li>Sun</li>
+				</ul>
+				<ul class="heatmap-calendar-boxes">
+					${boxes}
+				</ul>
+				</div>
+			`
+			el.insertAdjacentHTML("beforeend", html);
+		}
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		
 	}
 
 	onunload() {
@@ -91,47 +182,5 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
